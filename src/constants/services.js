5ea01ebdeadcddb9dc588a9acc0f30340b968e49
MeyerFrenchel <meyer.frenchel@gmail.com>
2021-01-19 10:31:19 +0200

    finished projects section

---
+++ src/constants/services.js
@@ -0,0 +1,22 @@
+import React from "react"
+import { FaCode, FaSketch, FaAndroid } from "react-icons/fa"
+export default [
+  {
+    id: 1,
+    icon: <FaCode className="icon-1 service-icon" />,
+    title: "Front End Development",
+    text: `I enjoy bringing ideas to life in the browser using HTML, CSS and Javascript, and building small and medium web apps with React or GatsbyJS. This also may include a CMS, API's or other integrations and frameworks.`,
+  },
+  {
+    id: 2,
+    icon: <FaSketch className="icon-2 service-icon" />,
+    title: "UI/UX Design",
+    text: `I value simple content structure, clean design patterns, and thoughtful interactions. Once the proper information architecture is in place, I design the visual layer to create a beautiful user experience.`,
+  },
+  {
+    id: 3,
+    icon: <FaAndroid className="service-icon icon-3" />,
+    title: "Product Ownership",
+    text: `I break the wall between people who know what to do and people who know how to do it. Market knowledge and excellent communication skills enable me to understand and anticipate the client’s needs , thus more effectively manage, lead and motivate the Development Team.`,
+  },
+]
